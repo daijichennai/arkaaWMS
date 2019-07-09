0fb46b6e38f0f3131b3a782b4b977943ebb4bafe
daijichennai <daijiworldtech@gmail.com>
2019-07-09 11:10:03 +0530

    changes in schemaAndData file

--- src/providers/commfunc/commfunc.ts
+++ src/providers/commfunc/commfunc.ts
@@ -4,8 +4,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CommfuncProvider {
 
-  //public domainURL : string = "http://www.videomemes.in/";
-  public domainURL : string = "http://localhost:49415/arkaWMS/";
+  public domainURL : string = "http://www.videomemes.in/";
+  //public domainURL : string = "http://localhost:49415/arkaWMS/";
+  //public domainURL: string = "http://192.168.1.2/sample/";
   constructor(public http: HttpClient) {
     
   }
